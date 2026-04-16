f2464eeb1bf2828e3ae1c69b2872279037f7d2e6
gopikriyayoga-create <gopikriyayoga@gmail>
2026-04-16 10:41:32 +0530

    Crypto dashboard running

--- client/src/Component/LoginSignup.jsx
+++ client/src/Component/LoginSignup.jsx
@@ -65,16 +65,19 @@ export default function AuthContainer() {
       }
     } else {
       try {
-        const response = await fetch("http://localhost:5000/api/auth/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
+        const response = await fetch(
+          "https://crypto-mern-q442.onrender.com/api/auth/register/auth/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: formData.email,
+              password: formData.password,
+            }),
           },
-          body: JSON.stringify({
-            email: formData.email,
-            password: formData.password,
-          }),
-        });
+        );
 
         const data = await response.json();
 
